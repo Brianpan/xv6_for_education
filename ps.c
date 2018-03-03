b8cb5c9de8ad589960564a77d41347552fc0d766
Brianpan <b97705021@ntu.edu.tw>
2018-03-02 20:15:20 -0800

    fix err

--- ps.c
+++ ps.c
@@ -7,22 +7,21 @@
 void ls_process()
 {
   uint pstruct_size = sizeof(struct uproc);
-  struct uproc *pstat = malloc(pstruct_size);
-  
   int pid;
   
   int i = 0;
   printf(1, "pname | pid | ppid | msize | pstate | iswait | killed \n");
   for(i=0;i<NPROC;i++)
   {
+    struct uproc *pstat = malloc(pstruct_size);
     // get pid
     pid = getpid2(i);
 
-    printf(1, "%d !!!", pid);
+    printf(1, "%d \n", pid);
     // printing
     if(getprocinfo(pid, pstruct_size, (void*)pstat) < 0)
       printf(1, "syscall err");
-
+      break;
     // printing process
     
     printf(1, "%s | ", pstat->name);
@@ -46,7 +45,9 @@ void ls_process()
     }
     printf(1, "%d | ", pstat->waiting);
     printf(1, "%d\n", pstat->killed);
-    }
+    
+    free(pstat);
+  }
 }
 
 int main(int argc, char* argv[])
